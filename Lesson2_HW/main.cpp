d3a9b17bf7b65d82dfdc2e194b6693daed76b05c
admin <inter.nafta32@gmail.com>
2021-01-30 22:35:11 +0200

    with minus

--- Lesson2_HW/main.cpp
+++ Lesson2_HW/main.cpp
@@ -12,20 +12,33 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 
-    int maxNum = 0, minNum = 9, num, separateNum;
+    int maxNum = 0, minNum = 9, num = 0, separateNum = 0;
     
     cout << "Enter your number:" << endl;
     cin >> num;
     
-    while (num)
-    {
-        separateNum = num % 10;
-        maxNum = maxNum > separateNum ? maxNum : separateNum;
-        minNum = minNum < separateNum ? minNum : separateNum;
-        cout << separateNum << " ";
-        num /= 10;
+    if (num > 0){
+        while (num)
+        {
+            separateNum = num % 10;
+            maxNum = maxNum > separateNum ? maxNum : separateNum;
+            minNum = minNum < separateNum ? minNum : separateNum;
+            cout << separateNum << " ";
+            num /= 10;
+        }
     }
-
+    else
+    {
+        num = -num;
+        while (num)
+        {
+            separateNum = num % 10;
+            maxNum = maxNum > separateNum ? maxNum : separateNum;
+            cout << separateNum << " ";
+            num /= 10;
+        }
+        minNum = -separateNum;
+    }
     cout << "\nOur min number is:" << minNum << endl;
     cout << "Our max number is:" << maxNum << endl;
     
